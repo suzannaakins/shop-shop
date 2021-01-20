2172aa03b435b9b7eebd025673b9084b4f534c1a
Garlic Fiend <suzannaakins@gmail.com>
2021-01-20 13:42:53 -0800

    url redirect

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -85,7 +85,7 @@ const resolvers = {
         payment_method_types: ['card'],
         line_items,
         mode: 'payment',
-        success_url: 'https://example.com/success?session_id={CHECKOUT_SESSION_ID}',
+        success_url: `${url}/success?session_id={CHECKOUT_SESSION_ID}`,
         cancel_url: `${url}/`
       });
 
