fac2e6945bddf9fede81ca0d4eb18e5c2e43ab7c
Garlic Fiend <suzannaakins@gmail.com>
2021-01-20 14:56:28 -0800

    add success page route

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,12 @@ import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { ApolloProvider } from '@apollo/react-hooks';
 import ApolloClient from 'apollo-boost';
-
 import Home from "./pages/Home";
 import Detail from "./pages/Detail";
 import NoMatch from "./pages/NoMatch";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
+import Success from './pages/Success';
 import Nav from "./components/Nav";
 import { StoreProvider } from "./utils/GlobalState";
 import OrderHistory from "./pages/OrderHistory";
@@ -37,6 +37,7 @@ function App() {
               <Route exact path="/signup" component={Signup} />
               <Route exact path="/orderHistory" component={OrderHistory} />
               <Route exact path="/products/:id" component={Detail} />
+              <Route exact path="/success" component={Success} />
               <Route component={NoMatch} />
             </Switch>
           </StoreProvider>
--- client/src/pages/Success.js
+++ client/src/pages/Success.js
@@ -3,3 +3,21 @@ import { useMutation } from '@apollo/react-hooks';
 import Jumbotron from '../components/Jumbotron';
 import { ADD_ORDER } from '../utils/mutations';
 import { idbPromise } from "../../utils/helpers";
+
+function Success() {
+    return (
+      <div>
+        <Jumbotron>
+          <h1>Success!</h1>
+          <h2>
+            Thank you for your purchase!
+          </h2>
+          <h2>
+            You will now be redirected to the homepage
+          </h2>
+        </Jumbotron>
+      </div>
+    );
+  };
+
+  export default Success;
